108d99c70c0abe2dc6d27739a17332312e8c405e
Anstetten <h.zsola@gmail.com>
2021-07-09 14:12:28 +0200

    Lab done with all bonuses

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// ITERATION 1
+
 
 function updateSubtotal(product) {
   
@@ -7,14 +7,12 @@ function updateSubtotal(product) {
   let quantity= product.querySelector('.quantity input').value;
   let subTotal= product.querySelector('.subtotal span');
 
-  console.log(quantity);
+ 
   let subtTotalAmount=price*quantity;
   subTotal.innerHTML = subtTotalAmount;
 
   return subtTotalAmount;
 
-
-  //... your code goes here
 }
 
 function calculateAll() {
@@ -22,18 +20,18 @@ function calculateAll() {
   let products = document.getElementsByClassName('product');
   let totalPrice=0;
   let totalPriceElement= document.querySelector('#total-value span')
-  console.log(products);
+  
   Array.from(products).forEach(product => {
     totalPrice+=updateSubtotal(product);    
   });
 
   totalPriceElement.innerHTML=totalPrice;
-
 }
 
-// ITERATION 4
+
 
 function removeProduct(event) {
+
   const target = event.currentTarget;
   let productToRemove= target.closest('.product');
   productToRemove.remove();
@@ -42,21 +40,71 @@ function removeProduct(event) {
   
 }
 
-// ITERATION 5
 
-function createProduct() {
-  //... your code goes here
+
+function createProduct(e) {
+  let button =e.target;
+  //closest does not seem to work as it is sibling
+  //so we get the whole new product card
+  let newProduct= e.target.closest('.create-product');
+
+  //get the different input nodes
+  let newProductName= newProduct.querySelector('input[type=text]').value;
+  let newProductPrice= newProduct.querySelector('input[type=number]').value;
+
+  //do not accept product without name or zero price (it makes us go bankrupt...)
+  if (newProductName===""||parseInt(newProductPrice)===0) { return}
+  
+  let newProductToInsert= document.createElement('tr')
+  newProductToInsert.classList.add("product");
+  //This is an ugly solution, but I did not want ot build it up step by step with createlemente
+  //plus I did not want to use templates as I did not want to modify the HTML
+  //+ I could not clone one of the products because it does not work if your cart is empty whern you load the window
+  newProductToInsert.innerHTML=`
+  <td class="name">
+            <span>${newProductName}</span>
+          </td>
+          <td class="price">$<span>${newProductPrice}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>`;
+
+  //get the table of products
+  let productTable= document.querySelector('#cart tbody');
+  
+  //attach new product to table
+
+  productTable.appendChild(newProductToInsert);
+
+  //wipe the new products field:
+  newProduct.querySelector('input[type=text]').value='';
+  newProduct.querySelector('input[type=number]').value=0;
+
+  //Refresh Remove Buttons
+  updateRemoveButtons();
+
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
+function updateRemoveButtons(){
   const removeButtons = document.getElementsByClassName('btn btn-remove');
-console.log(removeButtons);
   Array.from(removeButtons).forEach((button)=>{
     button.onclick=removeProduct;
   })
+}
+
+window.addEventListener('load', () => {
+
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+
+  updateRemoveButtons();
+
+  let createButton = document.getElementById("create");
+  createButton.onclick=createProduct;
 
   //... your code goes here
 });
